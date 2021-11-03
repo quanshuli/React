f945a107a32f06e2faf34a57cf643bd9aa4da466
quanshuli <quanshuli.dev@gmail.com>
2021-11-03 22:51:39 +1000

    part2

---
+++ part1/src/App-part1.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [value, setValue] = useState(10);
+  
+  const setToValue = (newValue) => () => setValue(newValue);
+  
+  return (
+    <div>
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
+    </div>
+  )
+}
+
+export default App;
--- part1/src/App.js
+++ part1/src/App.js
@@ -1,41 +1,17 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react'
+import Note from './components/Note'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-
+const App = ({ notes }) => {
   return (
     <div>
-      button press history: {props.allClicks.join(' ')}
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
   )
 }
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-const App = () => {
-  const [value, setValue] = useState(10);
-  
-  const setToValue = (newValue) => () => setValue(newValue);
-  
-  return (
-    <div>
-      {value}
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
-    </div>
-  )
-}
-
-export default App;
+export default App
