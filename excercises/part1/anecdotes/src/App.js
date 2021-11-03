bfb5d9e471f0114540fcf3b55eb56b5cdd50fd2c
quanshuli <quanshuli.dev@gmail.com>
2021-11-03 18:07:04 +1000

    finish

--- excercises/part1/anecdotes/src/App.js
+++ excercises/part1/anecdotes/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Button = (props) => (
+  //<button  text='next anecdote' />
+  <button onClick={props.handleClick}>{ props.text }</button>
+)
+
+const Mostvotes = props => (
+  <div>
+    <h1>Anecdote with most votes</h1>
+    <p>{props.text}</p>
+  </div>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  console.log(votes)
+
+  const randAnec = (stateFun) => () => {
+    const rand = Math.floor(Math.random() * (anecdotes.length + 1));
+    stateFun(rand)
+  }
+    
+  const increaseValue = (votes, selected) => () => {
+    const arr = [...votes]
+
+    arr[selected]++;
+
+    setVotes(arr)
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {anecdotes[selected]}
+      <br />
+      <div>has { votes[selected] } votes</div>
+      <Button handleClick={increaseValue(votes, selected)} text='vote' />
+      <Button handleClick={randAnec(setSelected)} text='next anecdote'/>
+      <Mostvotes text={anecdotes[votes.indexOf(Math.max(...votes))]} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
