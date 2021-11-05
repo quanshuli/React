996df4e5fee45d12e1d9a91382193a8d6f795f64
quanshuli <quanshuli.dev@gmail.com>
2021-11-05 22:18:52 +1000

    update

--- examples/src/App.js
+++ examples/src/App.js
@@ -7,13 +7,16 @@ const App = () => {
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
-  useEffect(() => {
+  const hook = () => {
     console.log("effect");
     axios.get("http://localhost:3001/notes").then((response) => {
       console.log("promise fulfilled");
       setNotes(response.data);
     });
-  }, []);
+  };
+
+  useEffect(hook, []);
+
   console.log("render", notes.length, "notes");
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
