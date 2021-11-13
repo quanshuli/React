7b4bbee919b33b345b45171438c153cbd6210059
quanshuli <quanshuli.dev@gmail.com>
2021-11-13 15:32:53 +1000

    Update Notes.js

--- examples/src/services/Notes.js
+++ examples/src/services/Notes.js
@@ -3,12 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { toggleImportanceOf } from "../reducers/noteReducer";
 
 const Note = ({ note, handleClick }) => {
-  /*
-    Note, responsible for rendering a single note, is very simple, 
-    and is not aware that the event handler it gets as props dispatches
-    an action. These kinds of components are called presentational in 
-    React terminology.
-    */
+  x;
 
   return (
     <li onClick={handleClick}>
@@ -21,13 +16,6 @@ const Note = ({ note, handleClick }) => {
 };
 
 const Notes = () => {
-  /*
-    Notes, on the other hand, is a container component, as it contains 
-    some application logic: it defines what the event handlers of the 
-    Note components do and coordinates the configuration of presentational 
-    components, that is, the Notes.
-    */
-
   const dispatch = useDispatch();
   const notes = useSelector((state) => state);
 
